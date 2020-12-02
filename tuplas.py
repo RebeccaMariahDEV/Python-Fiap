4b8cc555b7c99f1fbc4b17f9425b0d717a52c52f
RebeccaMariahDEV <71558611+RebeccaMariahDEV@users.noreply.github.com>
2020-12-02 14:58:11 -0300

    tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,17 +1,17 @@
 """
 Recapitulando tuplas:
 
-Tuplas são delimitadas por (), são como listas, podem ter varios dados dentro, porem seu diferencial é que são imutaveis,
+Tuplas são delimitadas por (), são como listas, podem ter vários dados dentro, porem seu diferencial é que são imutáveis,
 ou seja, não podemos alterar uma tupla; nem todos os métodos podem ser usados em tuplas, porem quando queremos um resultado
 ele pode retornar em formas de tupla
 
 Tuplas não aceitam alterações sobre os dados já inserido nela, mesmo não sendo muito utilizadas em programas devemos saber
-como usar e manipular, por exemplo, tuplas podem ser chaves de um dicionario, pois as chaves não vão mudar; vou explicar
+como usar e manipular, por exemplo, tuplas podem ser chaves de um dicionário, pois as chaves não vão mudar; vou explicar
 exemplo, se temos um dado na chave que são dois dados isso daria um erro, porem se for uma tupla contendo esses dados ai ok.
 
 Confuso? mas vou tentar melhorar
-
 """
+
 # utilizando tupas para respostas
 
 ips = {}
@@ -23,24 +23,24 @@ while resp == "S":
     resp = input("Digite <S> para continuar:\n ").upper()
 
 """
-Vamos ver o codigo, criamos um dicionario chamado ips. uma variavel chamada resp para controlar nosso laço e, 
-dentro do laço de repetição, vamos preencher o nosso dicionario, porem na chave tem dois valores, a primeira parte do 
-ip e a segunda dentro de uma tupla, e o dado do elemento que esta sendo adicionado.
+Vamos ver o código, criamos um dicionário chamado ips. uma variavel chamada resp para controlar nosso laço e, 
+dentro do laço de repetição, vamos preencher o nosso dicionário, porem na chave tem dois valores, a primeira parte do 
+ip e a segunda dentro de uma tupla, e o dado do elemento que está sendo adicionado.
 
 Agora vamos aproveitar esse código, printando ele
-
 """
+
 print("Exibindo ip´s: ")
 
 for ip in ips.keys():
     print(f"{ip[0]}.{ip[1]}")
 
 """
-Unimos as duas partes atraves do indice, ele vai colocar exatamente cada dado nesses posições
+Unimos as duas partes através do indice, ele vai colocar exatamente cada dado nesses posições.
 
-Agora vamos criar algo que só pegue os ip's de maquinas que estão no mesmo local e não os nomes das maquinas
-
+Agora vamos criar algo que só pegue os ip's de maquinas que estão no mesmo local e não os nomes das maquinas.
 """
+
 print("Exibindo máquinas com o mesmo endereço:\n")
 
 pesquisa = input("Digite os dois últimos octetos:\n")
@@ -51,9 +51,8 @@ for ip, nome in ips.items():
         print(nome)
 
 """
-Veja que pegamos os dados atraves do .items(), já vimos o que ele faz em!, agora vamos ver quais maquinas compõem uma 
+Veja que pegamos os dados através do .items(), já vimos o que ele faz em!, agora vamos ver quais maquinas compõem uma 
 mesma rede, pra isso vamos nos basear nos dois primeiros octetos do ip, vamos tentar.
-
 """
 print("Exibindo as máquinas que compõem uma mesma rede: ")
 
@@ -64,13 +63,12 @@ for ip, nome in ips.items():
         print(nome)
 
 """
-Até agora já conseguimos notar algumas coisas certo? já vimos como listas, tuplas e dicionarios funcionam, o que devemos
-fazer é saber o melhor momento de usar cada tipo de estutura de dados, não devemos escolher um ou outro, temos que saber
+Até agora já conseguimos notar algumas coisas certo? já vimos como listas, tuplas e dicionários funcionam, o que devemos
+fazer é saber o melhor momento de usar cada tipo de estrutura de dados, não devemos escolher um ou outro, temos que saber
 tudo por um todo.
 
-Outro exemplo é usarmos a função enumerete(), para listar cada dado dentro de algo, transformando eles em dicionarios ou
+Outro exemplo é usarmos a função enumerete(), para listar cada dado dentro de algo, transformando eles em dicionários ou
 em outras coisas 
-
 """
 usuarios = {}
 resp = "S"
@@ -97,10 +95,9 @@ for chave,dado in usuarios.items():
     print("Nível...: ", dado[1])
 
 """
-Até o momento a maior preocupação foi te mostrar as estruturas de dados basicas, como manipular e utilizar esses recursos,
-com esse basico você desenvolve algo, mas agora vem o divisor de águas, aqui vai ser o momento de se tornar algo a mais,
+Até o momento a maior preocupação foi te mostrar as estruturas de dados básicas, como manipular e utilizar esses recursos,
+com esse básico você desenvolve algo, mas agora vem o divisor de águas, aqui vai ser o momento de se tornar algo a mais,
 então estude bastante, treine o que foi feito até agora, se precisar veja a documentação do python.
 
-O proximo passo é banco de dados, aprender a mexer e manipular é mais complicado, porem nada é um bicho de 7 cabeças ok.
-
+O próximo passo é banco de dados, aprender a mexer e manipular é mais complicado, porem nada é um bicho de 7 cabeças ok.
 """
